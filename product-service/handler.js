155137c273c0357f4c76640d9d1e3ced12156236
SvetlankaKuzyaka <svetlanka-kuznecov@yandex.ru>
2023-11-01 16:21:23 +0300

    task-6: partially solved

--- product-service/handler.js
+++ product-service/handler.js
@@ -1,3 +1,4 @@
 export { getProductsList } from './handlers/getProductsList.js';
 export { getProductsById } from './handlers/getProductById.js';
 export { createProduct } from './handlers/createProduct.js';
+export { catalogBatchProcess } from './handlers/catalogBatchProcess.js';
